59c88d9e4eb58c502086d52bb5ff194be61163bb
WilliamGross <william@gross.nu>
2026-03-25 13:53:08 +0100

    Compilation error

--- src/main/java/Systementor/GithubActionsDemo/Calculator.java
+++ src/main/java/Systementor/GithubActionsDemo/Calculator.java
@@ -3,7 +3,7 @@ package Systementor.GithubActionsDemo;
 public class Calculator {
 
     public int add(int a, int b) {
-        return a + b;
+        return a + b
     }
 
     public int subtract(int a, int b) {
