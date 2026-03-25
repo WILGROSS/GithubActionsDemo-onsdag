7f3b8f5c63da3794240cf4fb8969c5f1d2369759
WilliamGross <william@gross.nu>
2026-03-25 13:37:42 +0100

    Fix the code, break the code

--- src/main/java/Systementor/GithubActionsDemo/Calculator.java
+++ src/main/java/Systementor/GithubActionsDemo/Calculator.java
@@ -3,7 +3,7 @@ package Systementor.GithubActionsDemo;
 public class Calculator {
 
     public int add(int a, int b) {
-        return a + b + 5;
+        return a + b
     }
 
     public int subtract(int a, int b) {
