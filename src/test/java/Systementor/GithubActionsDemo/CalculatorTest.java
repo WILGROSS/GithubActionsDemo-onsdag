7430de034bca1dd0d0173ce91540a3448aebabe7
WilliamGross <william@gross.nu>
2026-03-27 15:28:06 +0100

    Hej :)

--- src/test/java/Systementor/GithubActionsDemo/CalculatorTest.java
+++ src/test/java/Systementor/GithubActionsDemo/CalculatorTest.java
@@ -27,7 +27,7 @@ class CalculatorTest {
     void addShouldReturn3_withLogger(){
         logger.info("1- Logger: Starting test addShouldReturn3_withLogger");
 
-        int result = calculator.add(a, b) + 5;
+        int result = calculator.add(a, b);
         logger.info("2- Logger: Result from calculator: " + result);
 
         try {
